715cbebcc0940230294b436b13d726dacd7cd3f8
caozuba <nccuong.hn93@gmail.com>
2020-06-01 22:34:15 +0900

    split string first success

--- src/six_kyu/split_string/SplitString.java
+++ src/six_kyu/split_string/SplitString.java
@@ -1,20 +1,36 @@
 package six_kyu.split_string;
 
+import java.util.Arrays;
+
 public class SplitString {
   public static void main(String[] args) {
-
+    System.out.println("splitString(\"HellWorld\") = " + Arrays.toString(splitString("HellWorld")));
   }
 
+  /**
+   * Complete the solution so that it splits the string into pairs
+   * of two characters. If the string contains an odd number of
+   * characters then it should replace the missing second character of the
+   * final pair with an underscore ('_').
+   * <p>
+   * Examples:
+   * <p>
+   * StringSplit.solution("abc") // should return {"ab", "c_"}
+   * StringSplit.solution("abcdef") // should return {"ab", "cd", "ef"}
+   *
+   * @param s
+   * @return
+   */
   public static String[] splitString(String s) {
     int length = s.length();
     String[] strings = new String[length / 2 + length % 2];
-    for (int i = 0; i < strings.length - 1; i++) {
-      strings[i] = s.substring(i * 2, 2);
+
+    for (int i = 0; i < length / 2; i++) {
+      strings[i] = s.substring(i * 2, i * 2 + 2);
     }
-    if (length % 2 == 0) {
-      strings[strings.length - 1] = s.substring(s.length() - 1, 2);
-    } else {
-      strings[strings.length - 1] = s.substring(s.length() - 1, 1) + "_";
+
+    if (length % 2 == 1) {
+      strings[strings.length - 1] = s.substring(length - 1, length) + "_";
     }
 
     return strings;
