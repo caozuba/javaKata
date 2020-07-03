443656ad74e9ef21c242c0ebd25542fc0e475520
nccuong <nc.cuong@example.com>
2020-07-03 10:45:23 +0900

    SimpleAssembler first test

---
+++ src/five_kyu/SimpleAssembler.java
@@ -0,0 +1,13 @@
+package five_kyu;
+
+import java.util.Map;
+
+public class SimpleAssembler {
+  public static void main(String[] args) {
+
+  }
+
+  public static Map<String, Integer> interpret(String[] program) {
+    return null;
+  }
+}
---
+++ src/five_kyu/SimpleAssemblerTest.java
@@ -0,0 +1,28 @@
+package five_kyu;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleAssemblerTest {
+
+  @Test
+  public void simple_1() {
+    String[] program = new String[] { "mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a" };
+    Map<String, Integer> out = new HashMap<String, Integer>();
+    out.put("a", 1);
+    assertEquals(out, SimpleAssembler.interpret(program));
+  }
+
+  @Test
+  public void simple_2() {
+    String[] program = new String[] { "mov a -10", "mov b a", "inc a", "dec b", "jnz a -2" };
+    Map<String, Integer> out = new HashMap<String, Integer>();
+    out.put("a", 0);
+    out.put("b", -20);
+    assertEquals(out, SimpleAssembler.interpret(program));
+  }
+}
