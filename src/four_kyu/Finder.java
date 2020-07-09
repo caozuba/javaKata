6476542c1eb5dc07a0c5c53e9f32a59b880abb0d
caozuba <nccuong.hn93@gmail.com>
2020-07-09 22:15:48 +0900

    First passed

--- src/four_kyu/Finder.java
+++ src/four_kyu/Finder.java
@@ -14,38 +14,75 @@ public class Finder {
    * @param maze
    * @return
    */
-  public static boolean pathFinder(String maze) {
-    String[] mazeRows = maze.split("\n");
-    // Your code here!!
-    int currentPositionCol = 0;
-    int length = mazeRows.length;
-    for (int i = 0; i < length; i++) {
-      if (i == length - 1) {
-        break;
+  static int width;
+  static int height;
+  static int[][] maze; // The maze
+  static boolean[][] wasHere;
+  //  boolean[][] correctPath = new boolean[width][height]; // The solution to the maze
+  static int startX = 0, startY = 0; // Starting X and Y values of maze
+  static int endX, endY;     // Ending X and Y values of maze
+
+  public static boolean pathFinder(String mazeStr) {
+    generateMaze(mazeStr); // Create Maze (1 = path, 2 = wall)
+    wasHere = new boolean[width][height];
+    for (int row = 0; row < maze.length; row++)
+      // Sets boolean Arrays to default values
+      for (int col = 0; col < maze[row].length; col++) {
+        wasHere[row][col] = false;
+        //        correctPath[row][col] = false;
       }
-      String currentRow = mazeRows[i];
-      String nextRow = mazeRows[i + 1];
-      while (!canMoveNextRow(currentPositionCol, nextRow)) {
-        currentPositionCol++;
-        if (currentPositionCol == currentRow.length()) {
-          return false;
+    return recursiveSolve(startX, startY);
+    // Will leave you with a boolean array (correctPath)
+    // with the path indicated by true values.
+    // If b is false, there is no solution to the maze
+  }
+
+  private static void generateMaze(String mazeStr) {
+    String[] mazeRows = mazeStr.split("\n");
+    height = mazeRows.length;
+    width = mazeRows[0].length();
+    maze = new int[width][height];
+    endX = height - 1;
+    endY = width - 1;
+    for (int row = 0; row < height; row++) {
+      for (int col = 0; col < width; col++) {
+        if (mazeRows[row].substring(col, col + 1).equals("W")) {
+          maze[row][col] = 2;
+        } else {
+          maze[row][col] = 1;
         }
       }
     }
-    return !isLastPartHasWall(currentPositionCol, mazeRows[length - 1]);
   }
 
-  private static boolean canMoveRight(int currentColNum, String currentRow) {
-    String rightPoint = currentRow.substring(currentColNum, currentColNum + 1);
-    return !rightPoint.equals("W");
+  public static boolean recursiveSolve(int x, int y) {
+    if (x == endX && y == endY)
+      return true; // If you reached the end
+    if (maze[x][y] == 2 || wasHere[x][y])
+      return false;
+    // If you are on a wall or already were here
+    wasHere[x][y] = true;
+    if (x != 0) // Checks if not on left edge
+      if (recursiveSolve(x - 1, y)) { // Recalls method one to the left
+        //        correctPath[x][y] = true; // Sets that path value to true;
+        return true;
+      }
+    if (x != width - 1) // Checks if not on right edge
+      if (recursiveSolve(x + 1, y)) { // Recalls method one to the right
+        //        correctPath[x][y] = true;
+        return true;
+      }
+    if (y != 0)  // Checks if not on top edge
+      if (recursiveSolve(x, y - 1)) { // Recalls method one up
+        //        correctPath[x][y] = true;
+        return true;
+      }
+    if (y != height - 1) // Checks if not on bottom edge
+      if (recursiveSolve(x, y + 1)) { // Recalls method one down
+        //        correctPath[x][y] = true;
+        return true;
+      }
+    return false;
   }
 
-  private static boolean canMoveNextRow(int currentColNum, String nextRow) {
-    String nextRowPoint = nextRow.substring(currentColNum, currentColNum + 1);
-    return !nextRowPoint.equals("W");
-  }
-
-  private static boolean isLastPartHasWall(int currentCol, String lastRow) {
-    return lastRow.substring(currentCol).contains("W");
-  }
 }
--- src/four_kyu/FinderTest.java
+++ src/four_kyu/FinderTest.java
@@ -1,57 +1,198 @@
 package four_kyu;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.awt.*;
+import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static org.junit.Assert.assertEquals;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FinderTest {
 
-  @Test
-  public void sampleTests() {
-    //@formatter:off
-    String a = ".W.\n"+
-               ".W.\n"+
-               "...",
+  //@formatter:off
+    @Test public void fixedTests() {
 
-      b = ".W.\n"+
+      String a = ".W.\n"+
+        ".W.\n"+
+        "...",
+
+        b = ".W.\n"+
           ".W.\n"+
           "W..",
 
-      c = "......\n"+
+        c = "......\n"+
           "......\n"+
           "......\n"+
           "......\n"+
           "......\n"+
           "......",
 
-      d = "......\n"+
+        d = "......\n"+
           "......\n"+
           "......\n"+
           "......\n"+
           ".....W\n"+
-          "....W.",
-      e = ".....\n" +
-          ".W...\n" +
-          "..W..\n" +
-          "...W.\n" +
-          "...W.\n" ;
+          "....W.";
+
+      assertEquals(true,  Finder.pathFinder(a));
+      assertEquals(false, Finder.pathFinder(b));
+      assertEquals(true,  Finder.pathFinder(c));
+      assertEquals(false, Finder.pathFinder(d));
+    }
+
+
+    @Test public void moreTests() {
+
+      String a =
+        ".W...\n"+
+          ".W...\n"+
+          ".W.W.\n"+
+          "...W.\n"+
+          "...W.";
+      assertEquals(true, Finder.pathFinder(a));
+
+      a = ".W...\n"+
+        ".W...\n"+
+        ".W.W.\n"+
+        "...WW\n"+
+        "...W.";
+      assertEquals(false, Finder.pathFinder(a));
+
+      a = "..W\n"+
+        ".W.\n"+
+        "W..";
+      assertEquals(false, Finder.pathFinder(a));
+
+      a = ".WWWW\n"+
+        ".W...\n"+
+        ".W.W.\n"+
+        ".W.W.\n"+
+        "...W.";
+      assertEquals(true, Finder.pathFinder(a));
+
+      a = ".W...\n"+
+        "W....\n"+
+        ".....\n"+
+        ".....\n"+
+        ".....";
+      assertEquals(false, Finder.pathFinder(a));
+
+      a = ".W\n"+
+        "W.";
+      assertEquals(false, Finder.pathFinder(a));
+
+      a = ".";
+      assertEquals(true, Finder.pathFinder(a));
+    }
     //@formatter:on
-    assertTrue(Finder.pathFinder(a));
-    assertTrue(Finder.pathFinder(c));
-    assertFalse(Finder.pathFinder(d));
-    assertFalse(Finder.pathFinder(b));
-    assertTrue(Finder.pathFinder(e));
+
+  private static boolean DEBUG = false;
+
+  private Random rnd = new Random();
+
+  private int rand(int n) {
+    return rnd.nextInt(n);
   }
 
-  //  @Test
-  //  public void isLastPartHasWall() {
-  //    assertTrue(Finder.isLastPartHasWall(0, "...W.."));
-  //    assertFalse(Finder.isLastPartHasWall(3, "..W...."));
-  //  }
+  private int rand(int n, int m) {
+    return n + rnd.nextInt(m - n + 1);
+  }
 
-  //  @Test
-  //  public void canMoveNextRow() {
-  //    assertTrue(Finder.canMoveNextRow(2, "..W..."));
-  //  }
+  @Test
+  public void randomTests() {
+
+    int count = 0, TIMES = 20, RNG = 100;
+
+    for (int nTimes = 0; nTimes < TIMES; nTimes++) {
+      for (int n = 1; n < RNG + 1; n++) {
+
+        String maze = generateMaze(n);
+
+        boolean expected = KikicestKiTrouveLaSolution.pathFinder(maze);
+        assertEquals(expected, Finder.pathFinder(maze));
+
+        count += expected ? 1 : 0;
+      }
+    }
+
+    if (DEBUG)
+      System.out.println("" + count + "/" + (RNG * TIMES));
+  }
+
+  private String generateMaze(int n) {
+
+    int size = n * n, nWalls = rand(size / 4, size / 3);
+
+    Set<Integer> posWalls = new HashSet<>();
+    while (posWalls.size() < nWalls)
+      posWalls.add(rand(size));
+
+    final StringBuilder[] sbArr = IntStream.range(0, n)
+                                           .mapToObj(x -> new StringBuilder(
+                                             IntStream.range(0, n).mapToObj(y -> ".").collect(Collectors.joining())))
+                                           .toArray(StringBuilder[]::new);
+
+    posWalls.forEach(w -> {
+      int x = w / n, y = w % n;
+      sbArr[x].setCharAt(y, 'W');
+    });
+
+    sbArr[n - 1].setCharAt(n - 1, '.');        // first and last tile always free
+    sbArr[0].setCharAt(0, '.');
+
+    return Arrays.stream(sbArr).map(sb -> sb.toString()).collect(Collectors.joining("\n"));
+  }
+
+  private static final class KikicestKiTrouveLaSolution {
+
+    final private static List<Point> MOVES = Arrays.asList(new Point(1, 0), new Point(0, 1), new Point(0, -1),
+      new Point(-1, 0));
+
+    private static boolean pathFinder(String maze) {
+
+      int S = (int) Math.sqrt(maze.length()) - 1;
+      if (S == 0)
+        return true;
+
+      final Set<Point> bag = new HashSet<>();
+      int x = -1;
+      for (String line : maze.split("\n")) {
+        x++;
+        for (int y = 0; y < line.length(); y++)
+          if (line.charAt(y) == '.')
+            bag.add(new Point(x, y));
+      }
+      bag.remove(new Point(0, 0));
+
+      final Point end = new Point(S, S);
+      final boolean[] hasEnd = { false };
+      Set<Point> look = new HashSet<>(Arrays.asList(new Point(0, 0)));
+
+      while (!look.isEmpty()) {
+        if (hasEnd[0])
+          return true;
+        look = look.stream()
+                   .flatMap(p -> MOVES.stream().map(d -> new Point(p.x + d.x, p.y + d.y)))
+                   .distinct()
+                   .filter(p -> {
+                     if (p.equals(end))
+                       hasEnd[0] = true;
+                     if (bag.contains(p)) {
+                       bag.remove(p);
+                       return true;
+                     } else
+                       return false;
+                   })
+                   .collect(Collectors.toSet());
+      }
+      return false;
+    }
+  }
+
 }
