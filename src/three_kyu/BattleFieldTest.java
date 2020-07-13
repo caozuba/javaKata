d8e7635f08ccac4efae891a2be06ea65b688a5ba
nccuong <nc.cuong@example.com>
2020-07-13 16:03:11 +0900

    BattleField First Commit

---
+++ src/three_kyu/BattleFieldTest.java
@@ -0,0 +1,26 @@
+package three_kyu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BattleFieldTest {
+
+  private static int[][] battleField = {
+    { 1, 0, 0, 0, 0, 1, 1, 0, 0, 0 },
+    { 1, 0, 1, 0, 0, 0, 0, 0, 1, 0 },
+    { 1, 0, 1, 0, 1, 1, 1, 0, 1, 0 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
+    { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+  };
+
+  @Test
+  public void SampleTest() {
+    assertEquals(true, BattleField.fieldValidator(battleField));
+  }
+}
